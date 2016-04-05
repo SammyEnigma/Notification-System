14a3f3495c6eef2896ee365fbe1b36ba8e658757
aldonin <sxe-always@inbox.ru>
2016-04-05 12:39:07 +0300

    Add correct demo app closing, after notification widget disappearing

--- demo/main.cpp
+++ demo/main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     NotificationWidget *w = new NotificationWidget(QString("Title"), QString("Message"), QPixmap());
+    QObject::connect(w, SIGNAL(deleted()), &app, SLOT(quit()));
     w->show();
 
     return app.exec();
